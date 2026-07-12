0f18dfd029c5fb526ba4f806c6d2a40b4d1e3830
Lijo <lijov97@gmail.com>
2026-07-12 12:45:14 +0530

    tf change

--- deployment/values/stepshop-aps/production/terraform/backend.tf
+++ deployment/values/stepshop-aps/production/terraform/backend.tf
@@ -3,7 +3,7 @@ terraform {
     dynamodb_table = "stepora-production-terraform-state-tf"
 
     region = "us-east-1"
-    bucket = "stepora-production-terraform-state-tf"
+    bucket = "stepora-production-terraform-state-tf-bucket"
 
     key = "stepora/production/applications-v2/eks-v2/stepshop-aps.tfstate"
   }
